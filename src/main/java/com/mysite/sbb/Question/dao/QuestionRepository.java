c76c25ad82e8d00c69833823c9ed4bdafd4f0ec1
xanqus <sbk98031816@gmail.com>
2022-07-21 10:31:49 +0900

    내비게이션 바

--- src/main/java/com/mysite/sbb/Question/dao/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/Question/dao/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.mysite.sbb.Question.dao;
 
 import com.mysite.sbb.Question.domain.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.Question.service.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionService questionService;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용없음";
+
+			this.questionService.create(subject, content);
+		}
 	}
+
 
 }
